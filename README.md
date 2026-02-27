855891c1df26b0dba319a8a1c1d157ae38b4f1c8
burato42 <tmahmutov@gmail.com>
2026-02-27 12:45:37 +0100

    Reformat code and add argument parsing

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 Here is my solution for the coding challenge.
 
+This is a pure python implementation written in python 3.13.
+The program accepts a price catalog file in CSV format, mapping file in CSV format and generates a json file, containig the data grouped by brand and article.
+
+## Prerequisites
+Python 3.13 must be installed (earlier versions should work as well but they weren't tested)
+
+## Installation
+Not required.
+Extra libraries are not needed until you want to run tests. Tests are written using pytest, therefore this library will be required.
+
+## Run the tests
+
+## Running the program
+```commandline
+python -m src.transform --pricat <path_to_pricat.csv> --mappings <path_to_mappings.csv> --output <output_file.json>
+```
+
--- src/transform.py
+++ src/transform.py
@@ -1,17 +1,15 @@
+import argparse
 import csv
 import json
 from itertools import groupby
 from operator import itemgetter
 from typing import Any
 
-PRICE_CATALOG_FILE = "data/pricat_1.csv"
-MAPPING_FILE = "data/mappings_1.csv"
 
-
-def process_price_catalog(mapping):
+def process_price_catalog(price_catalog_file, mapping):
     result = []
-    with open(PRICE_CATALOG_FILE) as file:
-        catalog = csv.DictReader(file, delimiter=';')
+    with open(price_catalog_file) as file:
+        catalog = csv.DictReader(file, delimiter=";")
         for row in catalog:
             cur = {}
             for cat_field, value in row.items():
@@ -21,7 +19,7 @@ def process_price_catalog(mapping):
                     new_field_name = mapping[cat_field]["destination_type"]
                     value_mapping = mapping[cat_field]["values"]
                     cur[new_field_name] = value_mapping[value]
-                
+
             # process composite fields
             for source in mapping:
                 comp_filds = source.split("|")
@@ -29,21 +27,23 @@ def process_price_catalog(mapping):
                     comp_value = []
                     for source_field in comp_filds:
                         comp_value.append(row[source_field])
-                                          
+
                     new_field_name = mapping[source]["destination_type"]
                     value_mapping = mapping[source]["values"]
                     cur[new_field_name] = value_mapping["|".join(comp_value)]
-                  
+
             result.append(cur)
-           
+
     return result
 
-def get_mapping():
+
+def get_mapping(mapping_file):
     mapping: dict[str, Any] = dict()
-    
-    with open(MAPPING_FILE) as file:
-        mappings = csv.reader(file, delimiter=';')
-        header = next(mappings)
+
+    with open(mapping_file) as file:
+        mappings = csv.reader(file, delimiter=";")
+        # skip the header
+        next(mappings)
 
         for row in mappings:
             source, destination, source_type, destination_type = row
@@ -52,11 +52,11 @@ def get_mapping():
             else:
                 mapping[source_type] = {
                     "destination_type": destination_type,
-                    "values": {source: destination}
+                    "values": {source: destination},
                 }
-                
-    return mapping 
-            
+
+    return mapping
+
 
 def group_price_catalog(reformatted_catalog):
     grouped = {}
@@ -76,7 +76,9 @@ def group_price_catalog(reformatted_catalog):
 
         grouped_variations = []
         for record in variations:
-            del record["brand"] # remove the brand (as it's required in the task) and keep other fields 
+            del record[
+                "brand"
+            ]  # remove the brand (as it's required in the task) and keep other fields
             grouped_variations.append(record)
 
         variation = {"article_number": article_number, "variations": grouped_variations}
@@ -85,17 +87,26 @@ def group_price_catalog(reformatted_catalog):
     return {"catalog": grouped}
 
 
-def dump_to_json(data):
-    with open("output.json", "w") as f:
+def dump_to_json(data, output_file):
+    with open(output_file, "w") as f:
         json.dump(data, f)
 
 
-def main():
-    catalog_mapping = get_mapping()
-    reformated = process_price_catalog(catalog_mapping)
+def transform(pricat, mapping, output):
+    catalog_mapping = get_mapping(mapping)
+    reformated = process_price_catalog(pricat, catalog_mapping)
     aggregated = group_price_catalog(reformated)
-    dump_to_json(aggregated)
-    
-    
+    dump_to_json(aggregated, output)
+
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        prog="FashionCloud challenge",
+        description="The program takes the data from price catalog file, transforms it and saves the results into json.file",
+    )
+    parser.add_argument("--pricat", help="Price catalog csv file")
+    parser.add_argument("--mappings", help="Mappins csv file")
+    parser.add_argument("--output", help="Output json file")
+
+    args = parser.parse_args()
+    transform(args.pricat, args.mappings, args.output)
